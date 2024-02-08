7fe80e53967cf225259fc167d956e4ce43059145
bbjdbkvrv <nasheramarulloh@gmail.com>
2024-02-08 16:38:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const pageBox = document.querySelector('.page-box');
+const btnNext = document.querySelector('.btn-next');
+const btnBack = document.querySelector('.btn-back');
+const checkboxPass = document.querySelector('.checkbox-pass');
+const passwordInput = document.querySelector('.password');
+const loginTitle = document.querySelector('.loginTitle-text');
+const userEmail = document.querySelector('.user-email');
+const emailInput = document.querySelector('.email');
+
+btnNext.onclick = (e) => {
+    e.preventDefault();
+    pageBox.classList.add('active-pass');
+    setTimeout(() => passwordInput.focus(), 500);
+    loginTitle.textContent = 'Welcome';
+    userEmail.innerHTML = emailInput.Value;
+};
+
+btnBack.onclick = (e) => {
+    e.preventDefault();
+    pageBox.classList.remove('active-pass');
+    loginTitle.textContent = 'Login';
+    userEmail.innerHTML = 'Please login to use the platform';
+    emailInput.focus();
+};
+
+checkboxPass.onclick = () => {
+    if (checkboxPass.checked) {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+};
